259309392b2647eb33448417a57f0caa8a3399bf
FullStackDev-UK <workemailforworkstuff@gmail.com>
2019-09-14 00:56:35 +0100

    Blade views added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('index')
+
+@section('content')
+
+    <h1>Home page</h1>
+
+    <p><a href="profile">Profile</a></p>
+    
+@endsection
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,11 @@
+@extends('index')
+
+@section('content')
+
+    <h1>Profile</h1>
+    
+    <p>Username: {{$user->name}}</p>
+
+    <p><a href="home">Home</a></p>
+
+@endsection
